b49884a9a0f39c57b34d03af74e967067bce172a
Oscar Urrutia <oscarj.ugutierrez@gmail.com>
2023-09-27 10:33:08 -0500

    Rutas de Task definidas

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,3 +1,16 @@
 import { Router } from "express";
+import {
+  getTasks,
+  saveTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/task.controller.js";
 
 const router = Router();
+
+router.get("/", getTasks);
+router.post("/", saveTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+export default router;

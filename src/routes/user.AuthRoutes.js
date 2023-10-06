9d9abb93f727b34f0431a4809b43322658e1c290
Oscar Javier Urrutia Gutierrez <oscar.urrutia@jala.university>
2023-10-06 17:17:13 -0500

    Validaciones para los datos del usuario implementadas

--- src/routes/user.AuthRoutes.js
+++ src/routes/user.AuthRoutes.js
@@ -8,19 +8,22 @@ import {
   loginUser,
 } from "../controllers/user.controller.js";
 import { authenticateUser } from "../middlewares/authMiddleware.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import {
+  registerValidator,
+  loginValidator,
+} from "../validators/auth.validator.js";
 
 const router = express.Router();
 
 // CRUD
-router.post("/register", registerUser);
+router.post("/register", validateSchema(registerUser), registerUser);
 router.get("/users", getAllUsers);
 router.get("/users/:userId", getUserById);
 router.put("/users/:userId", updateUser);
 router.delete("/users/:userId", deleteUser);
 
 //Login
-router.post("/login", loginUser);
-
-
+router.post("/login", validateSchema(loginUser), loginUser);
 
 export default router;

d4b9ad7261b8ffd3fd31f2d34ca38063800f981d
Oscar Javier Urrutia Gutierrez <oscar.urrutia@jala.university>
2023-10-06 14:20:20 -0500

    Validaciones para tareas implementadas

---
+++ src/validators/task.validator.js
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const taskValidator = z.object(
+  {
+    title: z.string({ required_error: "El nombre de la tarea es requerido" }),
+  },
+  {
+    description: z.string({ required_error: "La descripción es requerida" }),
+  }
+);

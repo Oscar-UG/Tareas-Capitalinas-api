9678da2bf4b129f63234b29c34a3ef817339633a
Oscar Javier Urrutia Gutierrez <oscar.urrutia@jala.university>
2023-10-12 10:04:15 -0500

    Cors implementado

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from "express";
-import TaskRoute from './routes/task.route.js'
-import authRoutes from './routes/user.AuthRoutes.js'
-import ListRoute from './routes/list.route.js';
+import TaskRoute from "./routes/task.route.js";
+import authRoutes from "./routes/user.AuthRoutes.js";
+import ListRoute from "./routes/list.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api', TaskRoute)
-app.use('/api', authRoutes)
-app.use('/api', ListRoute)
+app.use("/api", TaskRoute);
+app.use("/api", authRoutes);
+app.use("/api", ListRoute);
 
 export default app;
